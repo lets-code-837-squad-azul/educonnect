7cfd625d60df3fadd2430f4b4b2be7c20f170d0c
Matheus Carneiro <76215464+mcarneir0@users.noreply.github.com>
2022-07-22 15:23:55 -0300

    Adicionado Create do Estado

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,6 +1,9 @@
 //  Arquivo de distribuição das rotas
 
+const EstadosRoutes = require('./Estados_routes');
 const express = require('express');
 const router = express.Router();
+
+router.use('/estados', EstadosRoutes);
 
 module.exports = router;
