d1b434af59fe78ebc2b03114a0f8833d1345f8e9
Pedro Souza <pedroworkspace@gmail.com>
2022-07-23 14:54:51 -0300

    getProfessor

---
+++ docs/js/cadastros/setProfessor.js
@@ -0,0 +1,39 @@
+// const fetch = require('fetch')
+
+const setProfessor = (nome, email, senha, telefone, cpf, nascimento, disciplina, etnia, genero, escolaridade) => {
+    const aluno = {
+        nome: nome,
+        email: email,
+        senha: senha,
+        telefone: telefone,
+        cpf: cpf,
+        nascimento: nascimento,
+        disciplina: disciplina,
+        etnia: etnia,
+        genero: genero, 
+        escolaridade: escolaridade
+    }
+    return aluno
+}
+
+const nome = document.getElementByName('nome').value 
+
+const email = document.getElementByName('email').value 
+
+const senha = document.getElementByName('senha').value
+
+const telefone = parseInt(document.getElementByName('telefone').value)
+
+const cpf = parseInt(document.getElementByName('cpf').value)
+
+const nascimento = document.getElementByName('nascimento').value
+
+const disciplina = document.getElementByName('disciplina').value
+
+const etnia = document.getElementsByName('etnia').value
+
+const genero = document.getElementsByName('genero').value
+
+const escolaridade = document.getElementsByName('escolaridade').value
+
+setProfessor(nome, email, senha, telefone, cpf, nascimento, etnia, genero, escolaridade)
