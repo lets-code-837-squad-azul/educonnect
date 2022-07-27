89a80f7c93f138b98f204f47e5b67ed31bf5fe34
DECIO SANTANA DE AGUIAR <decioagu@gmail.com>
2022-07-27 18:11:52 -0300

    2

--- frontend/js/home.js
+++ frontend/js/home.js
@@ -1,18 +1,20 @@
-let banco_de_dados_usuario = [{"cpf":123, "senha":"dsa"}]
+let banco_de_dados_usuario = [{"cpf":111, "senha":"aaa"},
+                              {"cpf":222, "senha":"bbb"},
+                              {"cpf":333, "senha":"ccc"}]
 
 function entrar(){
     const cpf = document.getElementById("CPF").value;
     const senha = document.getElementById("Senha").value;
-    console.log(cpf)
-    const teste = banco_de_dados_usuario.map((valor) => valor.cpf == cpf)
-    console.log(teste)
 
-    if(teste){
+    const teste = banco_de_dados_usuario.map((valor) => valor.cpf == cpf && valor.senha == senha)
+
+    
+    if(teste.indexOf(true)==-1){
       console.log("Falso");
-        /* document.querySelector("#erro").innerHTML = "Não foi identificado a senha ou o CPF de algum usuário!" */
+        document.querySelector("#erro").innerHTML = "Não foi identificado a senha ou o CPF de algum usuário!"
     } else{
       console.log("Verdadeiro");
-        /* document.querySelector("#erro").innerHTML =""; */
+        document.querySelector("#erro").innerHTML ="";
     }
 
 }
