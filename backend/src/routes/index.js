fad09076bbb84170a947c474ff9e82b893365b70
Matheus Carneiro <76215464+mcarneir0@users.noreply.github.com>
2022-07-27 13:37:56 -0300

    Adicionado Login por CPF e Senha

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -5,6 +5,7 @@ const Alunos_Routes = require('./Alunos_routes');
 const Monitor_Routes = require('./Monitor_routes');
 const Aula_Routes = require('./Aula_routes');
 const Disciplinas_Routes = require('./Disciplinas_routes');
+const Login_Routes = require('./Login_routes');
 const express = require('express');
 const router = express.Router();
 
@@ -13,5 +14,6 @@ router.use('/alunos', Alunos_Routes);
 router.use('/monitores', Monitor_Routes);
 router.use('/aulas', Aula_Routes);
 router.use('/disciplinas', Disciplinas_Routes);
+router.use('/login', Login_Routes);
 
 module.exports = router;
