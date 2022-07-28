b3b0c63bec7abf4d6db2d728484ec9a1d57c7842
Pedro Souza <pedroworkspace@gmail.com>
2022-07-28 18:34:46 -0300

    mudança de nome

--- frontend/js/cadastros/setAluno.js
+++ frontend/js/cadastros/setAluno.js
@@ -1,6 +1,27 @@
-// const fetch = require('fetch')
+const fetch = require('fetch')
 
-const setAluno = (nome, email, senha, telefone, cpf, nascimento, etnia, genero, escolaridade) => {
+const url = 'https://educonnect-lets-code-837.herokuapp.com/api/'
+
+const setAluno = () => {
+
+    const nome = document.getElementByName('nome').value 
+
+    const email = document.getElementByName('email').value 
+
+    const senha = document.getElementByName('senha').value
+
+    const telefone = document.getElementByName('telefone').value
+
+    const cpf = document.getElementByName('cpf').value
+
+    const nascimento = document.getElementByName('nascimento').value
+
+    const etnia = document.getElementsByName('etnia').value
+
+    const genero = document.getElementsByName('genero').value
+
+    const escolaridade = document.getElementsByName('escolaridade').value
+
     const aluno = {
         nome: nome,
         email: email,
@@ -12,25 +33,14 @@ const setAluno = (nome, email, senha, telefone, cpf, nascimento, etnia, genero, 
         genero: genero, 
         escolaridade: escolaridade
     }
+
+    fetch(`${url}alunos`, {method:'POST'}).then(response => response.json())
+    .then(data => {
+        console.log(data)
+    })
+
     return aluno
 }
 
-const nome = document.getElementByName('nome').value 
-
-const email = document.getElementByName('email').value 
-
-const senha = document.getElementByName('senha').value
-
-const telefone = document.getElementByName('telefone').value
-
-const cpf = document.getElementByName('cpf').value
-
-const nascimento = document.getElementByName('nascimento').value
-
-const etnia = document.getElementsByName('etnia').value
-
-const genero = document.getElementsByName('genero').value
-
-const escolaridade = document.getElementsByName('escolaridade').value
 
 setAluno(nome, email, senha, telefone, cpf, nascimento, etnia, genero, escolaridade)
