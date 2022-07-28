05b235061bc0e0f4ab90ec4577ba4e6f379d2347
Matheus Carneiro <76215464+mcarneir0@users.noreply.github.com>
2022-07-28 20:40:42 -0300

    Adicionado Script de Criação das Disciplinas

--- src/database/conexao.js
+++ src/database/conexao.js
@@ -6,7 +6,7 @@ const aplicar_relacionamentos = require('./relacionamentos');
 module.exports = async () => {
     try {
         aplicar_relacionamentos();
-        // await database.sync({ alter: true, logging: false });    //  Atualiza o banco de dados de acordo com as models
+        // await database.sync({ force: true, logging: false });    //  Atualiza o banco de dados de acordo com as models
         await database.authenticate();  //  Autentica o banco de dados
         return console.log('Conexão com o banco de dados realizada com sucesso!');
     }
--- src/repositories/Sandbox_rp.js
+++ src/repositories/Sandbox_rp.js
@@ -1,6 +1,7 @@
 //  TEST AREA
 
 const Estado = require('../models/Estado');
+const Disciplina = require('../models/Disciplina');
 
 //  Criar todos os estados
 const createTodosEstados = async (estados) => {
@@ -8,6 +9,13 @@ const createTodosEstados = async (estados) => {
     return res;
 }
 
+//  Criar todas as disciplinas
+const createTodasDisciplinas = async (disciplinas) => {
+    const res = await Disciplina.bulkCreate(disciplinas);
+    return res;
+}
+
 module.exports = {
     createTodosEstados,
+    createTodasDisciplinas,
 }
