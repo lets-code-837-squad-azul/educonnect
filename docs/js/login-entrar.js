d04c0aeb3a672f1b0f829d6412031892e9392867
Gabriela Sales <95177458+gabriela-gnsales@users.noreply.github.com>
2022-07-29 04:36:41 -0300

    Atualizando front-end

--- docs/js/login-entrar.js
+++ docs/js/login-entrar.js
@@ -1,14 +1,12 @@
+let aluno = sessionStorage.getItem("aluno");
+let monitor = sessionStorage.getItem("monitor");
 
-let aluno = sessionStorage.getItem('aluno');
-let monitor = sessionStorage.getItem('monitor');
+if (aluno == "false") {
+  document.getElementById("esconder_aluno").style.display = "none";
+}
 
+if (monitor == "false") {
+  document.getElementById("esconder_monitor").style.display = "none";
+}
 
-    if (aluno == 'false') {
-      document.getElementById("esconder_aluno").style.display = "none";
-    }
-
-    if (monitor == 'false') {
-      document.getElementById("esconder_monitor").style.display = "none";
-    }
-
-    document.querySelector("#nome").innerHTML = sessionStorage.getItem('nome');
+document.querySelector("#nome").innerHTML = sessionStorage.getItem("nome");
