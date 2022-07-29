ed94f63d27cbd0c6b6c131a70b16e63781e5e22f
Josuerodrigojr <98060884+Josuerodrigojr@users.noreply.github.com>
2022-07-29 01:47:17 -0300

    2

---
+++ docs/js/esqueci-senha.js
@@ -0,0 +1,26 @@
+const URL = 'https://educonnect-lets-code-837.herokuapp.com/api';
+
+
+
+
+function enviar(){
+    const email = document.getElementById('email').value;
+    
+    fetch(`${URL}/alunos/email/${email}`).then(response => response.json()).then(data=>{
+        if(data.message=="Aluno não encontrado"){
+
+        document.querySelector("#resposta").innerHTML = data.message
+        
+
+    } else {
+        document.querySelector("#resposta").innerHTML = "A senha foi enviada com sucesso para seu e-mail!!"
+
+    }});
+  
+
+
+
+
+
+    
+ }
--- docs/js/index.js
+++ docs/js/index.js
@@ -1,1 +1,37 @@
-alert("Arquivo JS carregado!");
+const ULR = "https://educonnect-lets-code-837.herokuapp.com/api";
+
+
+function entrar(){
+    const cpf = document.getElementById("CPF").value;
+    const senha = document.getElementById("Senha").value;
+
+    fetch(`${ULR}/login?cpf=${cpf}&senha=${senha}`)
+    .then(response => response.json())
+    .then((data )=> {
+
+      sessionStorage.setItem('aluno', data.eh_aluno)
+      sessionStorage.setItem('monitor', data.eh_monitor)
+
+      if (data.eh_aluno || data.eh_monitor) {
+        if(data.eh_aluno){
+        sessionStorage.setItem('nome', data.aluno.nome)
+        } else{
+          sessionStorage.setItem('nome', data.monitor.nome)
+        }
+        location.replace("./pages/login-entrar.html")
+      } else{
+        document.querySelector("#mensagem").innerHTML = data.message;
+      }
+    })
+
+
+
+
+
+
+
+
+
+
+
+}
