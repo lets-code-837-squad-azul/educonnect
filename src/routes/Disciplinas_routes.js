cde8596fdb1858a2d39efd11a4f0d040d2ce12d5
Matheus Carneiro <76215464+mcarneir0@users.noreply.github.com>
2022-07-29 11:11:15 -0300

    Adicionado Get Disciplina By ID

--- src/routes/Disciplinas_routes.js
+++ src/routes/Disciplinas_routes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 router.post('/', Disciplina_controller.createDisciplina);
 router.get('/', Disciplina_controller.getDisciplinas);
+router.get('/:id', Disciplina_controller.getDisciplinaById);
 router.patch('/:id', Disciplina_controller.updateDisciplina);
 router.delete('/:id', Disciplina_controller.deleteDisciplina);
 
