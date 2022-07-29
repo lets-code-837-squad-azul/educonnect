31b02a1440476c1c7f2999ceb849b86d7dfa35bf
Josuerodrigojr <98060884+Josuerodrigojr@users.noreply.github.com>
2022-07-29 17:30:38 -0300

    sda

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,14 +1,13 @@
-const ULR = "https://educonnect-lets-code-837.herokuapp.com/api";
+const URL = "https://educonnect-lets-code-837.herokuapp.com/api";
 
 
 function entrar(){
     const cpf = document.getElementById("CPF").value;
     const senha = document.getElementById("Senha").value;
 
-    fetch(`${ULR}/login?cpf=${cpf}&senha=${senha}`)
+    fetch(`${URL}/login?cpf=${cpf}&senha=${senha}`)
     .then(response => response.json())
     .then((data )=> {
-      console.log(data)
       sessionStorage.setItem('aluno', data.eh_aluno)
       sessionStorage.setItem('monitor', data.eh_monitor)
 
@@ -17,7 +16,10 @@ function entrar(){
           sessionStorage.setItem('nome', data.monitor.nome)
           sessionStorage.setItem("monitor_id", data.monitor.id)
 
-        } else{
+
+        }
+
+        if(data.eh_aluno){
           sessionStorage.setItem('nome', data.aluno.nome)
           sessionStorage.setItem("aluno_id", data.aluno.id)
 
