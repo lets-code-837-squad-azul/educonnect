ccb3f05a991a0004c7cd947a8bb30eb791a04192
Matheus Carneiro <76215464+mcarneir0@users.noreply.github.com>
2022-07-29 23:51:03 -0300

    Atualizado Listagem das Aulas

--- docs/js/tela-aluno-aulas.js
+++ docs/js/tela-aluno-aulas.js
@@ -1,20 +1,33 @@
+const URL = 'https://educonnect-lets-code-837.herokuapp.com/api'
 
+const aluno_id = sessionStorage.getItem('aluno_id')
 
-<div class="column">
-            <div class="card">
-                <label>Aula:<input type="text" id="aula" name="aula" disabled/></label>
-                <label>Matéria:<input type="text" id="materia" name="materia" disabled/></label>
-                <label>Monitor:<input type="text" id="monitor" name="monitor" disabled/></label>
-                <label><p id="data-inicio">Data de início:</p><input type="date" id="dataInicio" name="dataInicio" disabled/></label>
-                <label>Status:<input type="text" id="status" name="status" disabled/></label>
-                <p>Entre em contato com seu monitor:</p>
-                &ensp;
-                <a href=""><img src="https://img.icons8.com/material-outlined/35/000000/whatsapp--v1.png" alt="WhatsApp"></a>
-                &ensp;
-                <a href=""><img src="https://img.icons8.com/fluency-systems-regular/38/000000/ringer-volume.png" alt="Ligação"></a>
-                &ensp;
-                <a href=""><img src="https://img.icons8.com/material-outlined/35/000000/new-post.png" alt="Email"></a>
-                <br>
-                <button type="submit" onclick="deletarChat()" class="bt-deletar">Deletar</button>
-            </div>
-        </div>
+// GET request using fetch()
+fetch(`${URL}/aulas?aluno=${aluno_id}`)
+	.then(response => response.json())
+	.then(data => {
+        const aulas = data.aulas;
+        const cards = document.getElementById("aulasAgendadas");
+
+        cards.innerHTML = aulas.map(function(aula, index){
+            return `
+                <div class="column">
+                <div class="card">
+                    <label>Aula:<input type="text" id="aula" name="aula" value="${aula.id}" disabled/></label>
+                    <label>Disciplina:<input type="text" id="materia" name="materia" value="${aula.Disciplina.nome}" disabled/></label>
+                    <label>Monitor:<input type="text" id="monitor" name="monitor" value="${aula.Monitor.nome}" disabled/></label>
+                    <label><p id="data-inicio">Data de início:</p><input type="date" id="dataInicio" name="dataInicio" value="${aula.updatedAt.substring(0, 10)}" disabled/></label>
+                    <p>Entre em contato com seu monitor:</p>
+                    &ensp;
+                    <a href=""><img src="https://img.icons8.com/material-outlined/35/000000/whatsapp--v1.png" alt="WhatsApp"></a>
+                    &ensp;
+                    <a href=""><img src="https://img.icons8.com/fluency-systems-regular/38/000000/ringer-volume.png" alt="Ligação"></a>
+                    &ensp;
+                    <a href=""><img src="https://img.icons8.com/material-outlined/35/000000/new-post.png" alt="Email"></a>
+                    <br>
+                    <button type="submit" onclick="deletarChat()" class="bt-deletar">Deletar</button>
+                </div>
+                </div>
+            `
+        }).join("");
+    });
