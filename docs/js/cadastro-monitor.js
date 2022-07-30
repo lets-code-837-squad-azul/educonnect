18777d6ae4ac589f5d954ad17f5ed16003e8da6e
Gabriela Sales <95177458+gabriela-gnsales@users.noreply.github.com>
2022-07-29 23:50:50 -0300

    Atualizando projeto

---
+++ docs/js/cadastro-monitor.js
@@ -0,0 +1,35 @@
+const URL = 'https://educonnect-lets-code-837.herokuapp.com/api';
+
+function cadastrarAluno() {
+
+  const nome = document.getElementById('nome').value;
+  const email = document.getElementById('email').value;
+  const senha = document.getElementById('senha').value;
+  const telefone = document.getElementById('telefone').value;
+  const cpf = document.getElementById('cpf').value;
+  const estado_id = document.getElementById('estado').value;
+  const data_de_nascimento = document.getElementById('data_de_nascimento').value;
+  const etnia = document.getElementById('etnia').value;
+  const genero = document.getElementById('genero').value;
+  const escolaridade = document.getElementById('escolaridade').value;
+
+
+
+fetch(`${URL}/monitores/`,{method:"POST", body:JSON.stringify({
+    nome: nome,
+    email: email,
+    senha: senha,
+    telefone: telefone,
+    cpf: cpf,
+    estado_id: estado_id,
+    data_de_nascimento: data_de_nascimento,
+    etnia: etnia,
+    genero: genero,
+    escolaridade: escolaridade
+  }),headers: {
+      "Content-type": "application/json; charset=UTF-8"
+  }
+})
+.then(response => response.json())
+.then(data => console.log(data))
+}
--- docs/js/login-entrar.js
+++ docs/js/login-entrar.js
@@ -1,12 +1,14 @@
-let aluno = sessionStorage.getItem("aluno");
-let monitor = sessionStorage.getItem("monitor");
+let aluno = sessionStorage.getItem('aluno');
+let monitor = sessionStorage.getItem('monitor');
 
-if (aluno == "false") {
-  document.getElementById("esconder_aluno").style.display = "none";
-}
 
-if (monitor == "false") {
-  document.getElementById("esconder_monitor").style.display = "none";
-}
+    if (aluno == 'false') {
+      document.getElementById("esconder_aluno").style.display = "none";
+    }
 
-document.querySelector("#nome").innerHTML = sessionStorage.getItem("nome");
+    if (monitor == 'false') {
+      document.getElementById("esconder_monitor").style.display = "none";
+      
+    }
+
+    document.querySelector("#nome").innerHTML = sessionStorage.getItem('nome');
